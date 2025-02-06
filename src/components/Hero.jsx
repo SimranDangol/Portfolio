41173f1625c3f27279216e2dcc908c4e9e26300c
Simran Dangol <simrandangol10@gmail.com>
2025-02-06 11:17:17 +0545

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,17 +23,7 @@ const Hero = () => {
             <p className="mb-4">
             I'm an aspiring web developer with a solid understanding of React, Node.js, Express, and modern web technologies. I enjoy creating engaging and functional websites that aim to address real-world challenges.
             </p>
-            <button className="flex items-center gap-2 px-3 py-2 text-white bg-black rounded-md w-max">
-              <HiMiniArrowDownTray size={20} />
-              <a
-                href="https://drive.google.com/file/d/1kxE2hjffw9O5Pc4o-VmIwh4WMz_xKM5o/view?usp=drive_link"
-                download
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Download CV
-              </a>
-            </button>
+          
           </div>
           <div className="relative flex items-end justify-center md:w-1/2">
             <img src={a} alt="Hero" className="lg:h-[90vh] h-96" />
